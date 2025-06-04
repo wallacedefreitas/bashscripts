20ea8ef374c2a69ae238fa1008987803bc173eae
wallacedefreitas <wallacedefreitasrj@gmail.com>
2025-06-04 17:03:38 -0300

    aumentando disco vm10gb

--- ubuntupromox.sh
+++ ubuntupromox.sh
@@ -181,7 +181,7 @@ pvesm alloc $STORAGE $VMID $DISK0 4M 1>&/dev/null
 qm importdisk $VMID ${FILE} $STORAGE ${DISK_IMPORT:-} 1>&/dev/null
 qm set $VMID \
   -efidisk0 ${DISK0_REF}${FORMAT} \
-  -scsi0 ${DISK1_REF},${DISK_CACHE}${THIN}size=2G \
+  -scsi0 ${DISK1_REF},${DISK_CACHE}${THIN}size=50G \
   -ide2 ${STORAGE}:cloudinit \
   -boot order=scsi0 \
   -serial0 socket \
